88841f254fc71a3af7cd4132dc904ce489472ddf
Ryan Lewis <cosmotron@gmail.com>
2012-04-29 23:57:36 -0400

    Added copyright header to files

--- src/Impetus/AppBundle/Service/AnalyticsService.php
+++ src/Impetus/AppBundle/Service/AnalyticsService.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the ImpetusAppBundle
+ *
+ * (c) Ryan Lewis <lewis.ryan.j@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Impetus\AppBundle\Service;
 
 use Impetus\AppBundle\Entity\Year;
